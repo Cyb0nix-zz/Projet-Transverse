5156c0572d23f3b4f631649388b4a9653a09503a
Cédric <cedricyoganathan@gmail.com>
2021-03-04 11:42:58 +0100

    init

---
+++ Main.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.locals import*
+
+pygame.init()
+
+win = pygame.display.set_mode((1280,720))
+
+pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+
+
+pygame.quit()
+
+
