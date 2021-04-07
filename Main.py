c938cf1c973f73af4d2233e21f1e57e2a6b757b1
Cédric <cedricyoganathan@gmail.com>
2021-04-07 22:30:31 +0200

    edit hitbox

--- Main.py
+++ Main.py
@@ -1,29 +1,30 @@
-import pygame,sys
-from pygame.locals import*
+import pygame, sys
+from pygame.locals import *
 
 clock = pygame.time.Clock()
 
 pygame.init()
 
-WINDOW_SIZE = (1280,720)
+WINDOW_SIZE = (1280, 720)
 
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
-display = pygame.Surface((1000,544))
+display = pygame.Surface((1000, 544))
 
 pygame.display.set_caption("The Shadow of the past")
 
-player_img = pygame.image.load('Caracter\player.png')
+player_img = pygame.image.load('Caracter/player.png')
 background = pygame.image.load('textures/Background/Map_ville.png')
 
 wall_block = pygame.image.load('textures/wall_building.png')
 windows_block = pygame.image.load('textures/building_night_light_on.png')
 TILE_SIZE = windows_block.get_width()
 
-true_scroll = [0,0]
+true_scroll = [0, 0]
+
 
 def load_map(path):
-    f = open(path+ '.txt','r')
+    f = open(path + '.txt', 'r')
     data = f.read()
     f.close()
     data = data.split('\n')
@@ -32,7 +33,9 @@ def load_map(path):
         game_map.append(list(row))
     return game_map
 
+
 game_map = load_map('map')
+
 
 def collision_test(rect, tiles):
     hit_list = []
@@ -40,6 +43,7 @@ def collision_test(rect, tiles):
         if rect.colliderect(tile):
             hit_list.append(tile)
     return hit_list
+
 
 def move(rect, movement, tiles):
     collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
@@ -56,7 +60,7 @@ def move(rect, movement, tiles):
     rect.y += movement[1]
     hit_list = collision_test(rect, tiles)
     for tile in hit_list:
-        if movement[1]> 0:
+        if movement[1] > 0:
             rect.bottom = tile.top
             collision_types['bottom'] = True
         elif movement[1] < 0:
@@ -65,31 +69,25 @@ def move(rect, movement, tiles):
     return rect, collision_types
 
 
-
-
-
-
 move_right = False
 move_left = False
 
 player_y_momentum = 0
 air_timer = 0
 
-
-player_box = pygame.Rect(400, 500, player_img.get_width(), player_img.get_height())
+player_box = pygame.Rect(400, 500, player_img.get_width(), player_img.get_height()-10)
 test_rect = pygame.Rect(100, 100, 100, 50)
 
 while True:
     display.fill((146, 244, 255))
 
-    true_scroll[0] += (player_box.x-true_scroll[0]-400)/20
-    true_scroll[1] += (player_box.y-true_scroll[1]-390)/20
+    true_scroll[0] += (player_box.x - true_scroll[0] - 400) / 20
+    true_scroll[1] += (player_box.y - true_scroll[1] - 390) / 20
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
 
-    display.blit(background,(0-scroll[0]*0.15,-250-scroll[1]*0.15))
-
+    display.blit(background, (0 - scroll[0] * 0.15, -250 - scroll[1] * 0.15))
 
     tile_rects = []
     y = 0
@@ -98,22 +96,22 @@ while True:
         x = 0
         for tile in row:
             if tile == '1':
-                display.blit(wall_block, (x * TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                display.blit(wall_block, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
             if tile == '2':
-                display.blit(windows_block, (x * TILE_SIZE-scroll[0], y*TILE_SIZE-scroll[1]))
+                display.blit(windows_block, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
             if tile != '0':
-                tile_rects.append(pygame.Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
-            x+= 1
-        y+= 1
+                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
 
-    player_movement = [0,0]
+    player_movement = [0, 0]
 
     if move_right:
         player_movement[0] += 3
     if move_left:
         player_movement[0] -= 3
     player_movement[1] += player_y_momentum
-    player_y_momentum += 0.4
+    player_y_momentum += 0.5
     if player_y_momentum > 3:
         player_y_momentum = 3
 
@@ -124,10 +122,7 @@ while True:
     else:
         air_timer += 1
 
-    display.blit(player_img, (player_box.x-scroll[0],player_box.y-scroll[1]))
-
-
-
+    display.blit(player_img, (player_box.x - scroll[0], player_box.y - scroll[1]))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -140,7 +135,7 @@ while True:
                 move_left = True
             if event.key == K_UP:
                 if air_timer < 6:
-                    player_y_momentum = -9
+                    player_y_momentum = -10
         if event.type == KEYUP:
             if event.key == K_RIGHT:
                 move_right = False
@@ -148,14 +143,6 @@ while True:
                 move_left = False
 
     surf = pygame.transform.scale(display, WINDOW_SIZE)
-    screen.blit(surf,(0,0))
+    screen.blit(surf, (0, 0))
     pygame.display.update()
-    clock.tick(120)
-
-
-
-
-
-
-
-
+    clock.tick(240)
