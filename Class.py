2d6b00a4577b8a36e1e1ea23fd7c66b94f7b2392
Cédric <cedricyoganathan@gmail.com>
2021-04-09 19:47:06 +0200

    mouvement + hitbox finalisé

--- Class.py
+++ Class.py
@@ -4,17 +4,18 @@ from pygame.locals import *
 
 class player:
 
-    def __init__(self, screen, tile_rects):
+    def __init__(self):
         self.player_img = pygame.image.load('Caracter/player.png')
-        self.movement = [0,0]
+        self.player_box = pygame.Rect(0, 0, self.player_img.get_width(), self.player_img.get_height() - 10)
+
+    def setLocation(self, x, y):
+        self.player_box.y = y
+        self.player_box.x = x
+
+    def move(self, movement, tile_rects):
+        self.movement = movement
+        self.collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
         self.tiles = tile_rects
-
-
-    def setLocation(self,x,y):
-        self.player_box = pygame.Rect(x, y, self.player_img.get_width(), self.player_img.get_height() - 10)
-
-    def move(self):
-        collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
 
         self.player_box.x += self.movement[0]
 
@@ -26,10 +27,11 @@ class player:
         for tile in hit_list:
             if self.movement[0] > 0:
                 self.player_box.right = tile.left
-                collision_types['right'] = True
+                self.collision_types['right'] = True
             elif self.movement[0] < 0:
                 self.player_box.left = tile.right
-                collision_types['left'] = True
+                self.collision_types['left'] = True
+
         self.player_box.y += self.movement[1]
 
         for tile in self.tiles:
@@ -39,7 +41,11 @@ class player:
         for tile in hit_list:
             if self.movement[1] > 0:
                 self.player_box.bottom = tile.top
-                collision_types['bottom'] = True
+                self.collision_types['bottom'] = True
             elif self.movement[1] < 0:
                 self.player_box.top = tile.bottom
-                collision_types['top'] = True
+                self.collision_types['top'] = True
+
+    def update(self, display, scroll):
+
+        display.blit(self.player_img, (self.player_box.x - scroll[0], self.player_box.y - scroll[1]))
--- Main.py
+++ Main.py
@@ -6,11 +6,11 @@ clock = pygame.time.Clock()
 
 pygame.init()
 
-WINDOW_SIZE = (1280, 720)
+WINDOW_SIZE = (1280,720)
 
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
-display = pygame.Surface((1000, 544))
+display = pygame.Surface((1280,704))
 
 pygame.display.set_caption("The Shadow of the past")
 
@@ -21,7 +21,9 @@ wall_block = pygame.image.load('textures/wall_building.png')
 windows_block = pygame.image.load('textures/building_night_light_on.png')
 TILE_SIZE = windows_block.get_width()
 
-true_scroll = [0, 0]
+player = player()
+
+true_scroll = [0,0]
 
 def load_map(path):
     f = open(path + '.txt', 'r')
@@ -33,41 +35,9 @@ def load_map(path):
         game_map.append(list(row))
     return game_map
 
-
 game_map = load_map('map')
 
-
-def collision_test(rect, tiles):
-    hit_list = []
-    for tile in tiles:
-        if rect.colliderect(tile):
-            hit_list.append(tile)
-    return hit_list
-
-
-def move(rect, movement, tiles):
-    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
-
-    rect.x += movement[0]
-    hit_list = collision_test(rect, tiles)
-    for tile in hit_list:
-        if movement[0] > 0:
-            rect.right = tile.left
-            collision_types['right'] = True
-        elif movement[0] < 0:
-            rect.left = tile.right
-            collision_types['left'] = True
-    rect.y += movement[1]
-    hit_list = collision_test(rect, tiles)
-    for tile in hit_list:
-        if movement[1] > 0:
-            rect.bottom = tile.top
-            collision_types['bottom'] = True
-        elif movement[1] < 0:
-            rect.top = tile.bottom
-            collision_types['top'] = True
-    return rect, collision_types
-
+player.setLocation(400, 500)
 
 move_right = False
 move_left = False
@@ -75,23 +45,19 @@ move_left = False
 player_y_momentum = 0
 air_timer = 0
 
-player_box = pygame.Rect(400, 500, player_img.get_width(), player_img.get_height()-10)
-test_rect = pygame.Rect(100, 100, 100, 50)
-
 while True:
+
     display.fill((146, 244, 255))
 
-    true_scroll[0] += (player_box.x - true_scroll[0] - 400) / 20
-    true_scroll[1] += (player_box.y - true_scroll[1] - 390) / 20
+    true_scroll[0] += (player.player_box.x - true_scroll[0] - 400) / 20
+    true_scroll[1] += (player.player_box.y - true_scroll[1] - 555) / 20
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
 
-    display.blit(background, (0 - scroll[0] * 0.15, -250 - scroll[1] * 0.15))
+    display.blit(background, (0 - scroll[0] * 0.15, -200 - scroll[1] * 0.15))
 
     tile_rects = []
-
-
     y = 0
     for row in game_map:
         x = 0
@@ -112,19 +78,18 @@ while True:
     if move_left:
         player_movement[0] -= 3
     player_movement[1] += player_y_momentum
-    player_y_momentum += 0.5
+    player_y_momentum += 0.6
     if player_y_momentum > 3:
         player_y_momentum = 3
 
-    player_box, collisions = move(player_box, player_movement, tile_rects)
-    if collisions['bottom']:
+    player.move(player_movement, tile_rects)
+    if player.collision_types['bottom']:
         player_y_momentum = 0
         air_timer = 0
     else:
         air_timer += 1
 
-    display.blit(player_img, (player_box.x - scroll[0], player_box.y - scroll[1]))
-
+    player.update(display, scroll)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -136,7 +101,7 @@ while True:
                 move_left = True
             if event.key == K_UP:
                 if air_timer < 6:
-                    player_y_momentum = -10
+                    player_y_momentum = -7
         if event.type == KEYUP:
             if event.key == K_RIGHT:
                 move_right = False
@@ -146,4 +111,4 @@ while True:
     surf = pygame.transform.scale(display, WINDOW_SIZE)
     screen.blit(surf, (0, 0))
     pygame.display.update()
-    clock.tick(240)
+    clock.tick(120)
