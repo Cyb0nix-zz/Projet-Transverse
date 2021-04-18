8d5140537086ad9a6abc3b4d8d84dcf21212d653
Cédric <cedric.yoganathan@efrei.net>
2021-04-18 18:34:37 +0200

    add level editor

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 import sys
 from Class import *
+import csv
+import os
+from tkinter import messagebox
+import tkinter as tk
 
 WINDOW_SIZE = (1280, 720)
 TILE_SIZE = 32
+FPS = 120
 
 clock = pygame.time.Clock()
 pygame.init()
@@ -140,8 +145,200 @@ def Game():
         surf = pygame.transform.scale(display, WINDOW_SIZE)
         pygame.display.update()
         screen.blit(surf, (0, 0))
-        clock.tick(120)
+        clock.tick(FPS)
+
+def LevelEditor():
+    # Taille de la fenêtre (avec taille marges)
+    LOWER_MARGIN = 100
+    SIDE_MARGIN = 500
+    SCREEN_WIDTH = 1281
+    SCREEN_HEIGHT = 897
 
 
+    # Variables
+    ROWS = 29
+    MAX_COLS = 312
+    TILE_SIZE = 32
+    TILE_TYPES = 134
+    level = 0
+    scroll_left = False
+    scroll_right = False
+    scroll = 0
+    scroll_speed = 1
+    current_tile = 0
+    grid = 0
 
-Game()
+    display = pygame.display.set_mode((SCREEN_WIDTH + SIDE_MARGIN, SCREEN_HEIGHT))
+
+    # Background
+    bg_0 = pygame.image.load('LevelEditor/bg/0.png').convert_alpha()
+    bg = bg_0.convert_alpha()
+
+    # Créer les différentes tile
+    img_list = []
+    for x in range(TILE_TYPES):
+        img = pygame.image.load(f'LevelEditor/32x32/{x}.png').convert_alpha()
+        img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+        img_list.append(img)
+
+    # Boutton pour sauvegarder et charger les niveaux
+    save_btn = pygame.image.load('LevelEditor/save_btn.png').convert_alpha()
+    load_btn = pygame.image.load('LevelEditor/load_btn.png').convert_alpha()
+    grid_btn = pygame.image.load('LevelEditor/grid_btn.png').convert_alpha()
+    hide_btn = pygame.image.load('LevelEditor/hide_btn.png').convert_alpha()
+    trash_btn = pygame.image.load('LevelEditor/trash.png').convert_alpha()
+
+    # Couleurs
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (200, 25, 25)
+    GREEN = (144, 201, 120)
+
+    font = pygame.font.SysFont('Futura', 30)
+
+    # Liste vide de Tiles
+    world_data = []
+    for row in range(ROWS):
+        r = [-1] * MAX_COLS
+        world_data.append(r)
+
+    def draw_text(text, font, text_col, x, y):
+        img = font.render(text, True, text_col)
+        display.blit(img, (x, y))
+
+    # Dessiner background
+    def draw_bg(bgd):
+        display.fill(BLACK)
+        width = bgd.get_width()
+        for x in range(1):
+            display.blit(bgd, ((x * width) - scroll, 0))
+
+    # Dessiner les grid pour placer les éléments
+    def draw_grid(val):
+        # Ligne verticales
+        if val == 1:
+            for x in range(MAX_COLS + 1):
+                pygame.draw.line(display, WHITE, (x * TILE_SIZE - scroll, 0), (x * TILE_SIZE - scroll, SCREEN_HEIGHT))
+            # Ligne horizontales
+            for y in range(ROWS + 1):
+                pygame.draw.line(display, WHITE, (0, y * TILE_SIZE), (SCREEN_WIDTH, y * TILE_SIZE))
+        else:
+            pass
+
+    def del_function(level):
+        try:
+            msgbox = messagebox.askquestion('Delete level ', 'Voulez-vous vraiment supprimer le niveau ? ',
+                                            icon="warning")
+            if msgbox == 'yes':
+                os.remove(f'LevelEditor/level{level}_data.txt')
+            else:
+                pass
+        except FileNotFoundError:
+            pass
+
+    # Dessiner les tiles dans le monde
+    def draw_world():
+        for y, row in enumerate(world_data):
+            for x, tile in enumerate(row):
+                if tile >= 0:
+                    display.blit(img_list[tile], (x * TILE_SIZE - scroll, y * TILE_SIZE))
+
+    # Boutons paramètres
+    button_list = []
+    button_col = 0
+    button_row = 0
+
+    for i in range(len(img_list)):
+        tile_button = Button(SCREEN_WIDTH + (50 * button_col) + 50, 50 * button_row + 50, img_list[i], 1)
+        button_list.append(tile_button)
+        button_col += 1
+        if button_col == 9:
+            button_row += 1
+            button_col = 0
+
+    root = tk.Tk()
+    root.withdraw()
+    run = True
+    while run:
+        clock.tick(FPS)
+        draw_text(f'{FPS}', font, WHITE, 1200, 870)
+        draw_bg(bg)
+        draw_world()
+        draw_grid(grid)
+        pygame.draw.rect(display, GREEN, (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
+        save_button = Button(SCREEN_WIDTH + 30, 845, save_btn, 1)
+        load_button = Button(SCREEN_WIDTH + 120, 845, load_btn, 1)
+        hide_button = Button(SCREEN_WIDTH + 210, 845, hide_btn, 1)
+        grid_button = Button(SCREEN_WIDTH + 300, 845, grid_btn, 1)
+        trash_button = Button(SCREEN_WIDTH + 430, 840, trash_btn, 1)
+        draw_text(f'Level: {level} ', font, WHITE, 1190, 870)
+        draw_text(f'FPS:{FPS} ', font, WHITE, 1190, 830)
+        if trash_button.draw(display):
+            del_function(level)
+        if save_button.draw(display):
+            with open(f'LevelEditor/level{level}_data.txt', 'w', newline='') as txtfile:
+                writer = csv.writer(txtfile, delimiter=',')
+                for row in world_data:
+                    writer.writerow(row)
+        if load_button.draw(display):
+            scroll = 0
+            try:
+                with open(f'LevelEditor/level{level}_data.txt', newline='') as txtfile:
+                    reader = csv.reader(txtfile, delimiter=',')
+                    for x, row in enumerate(reader):
+                        for y, tile in enumerate(row):
+                            world_data[x][y] = int(tile)
+            except FileNotFoundError:
+                pass
+        if hide_button.draw(display):
+            grid = 0
+        if grid_button.draw(display):
+            grid = 1
+        button_count = 0
+        for button_count, i in enumerate(button_list):
+            if i.draw(display):
+                current_tile = button_count
+        rectangle = pygame.draw.rect(display, WHITE, button_list[current_tile].rect, 3)
+        if scroll_left and scroll > 0:
+            scroll -= 5 * scroll_speed
+        if scroll_right and scroll < (MAX_COLS * TILE_SIZE) - SCREEN_WIDTH:
+            scroll += 5 * scroll_speed
+
+        pos = pygame.mouse.get_pos()
+        x = (pos[0] + scroll) // TILE_SIZE
+        y = pos[1] // TILE_SIZE
+        if pos[0] < SCREEN_WIDTH and pos[1] < SCREEN_HEIGHT:
+            if pygame.mouse.get_pressed()[0] == 1:
+                if world_data[y][x] != current_tile:
+                    world_data[y][x] = current_tile
+            if pygame.mouse.get_pressed()[2] == 1:
+                world_data[y][x] = -1
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    level += 1
+                if event.key == pygame.K_DOWN and level > -1:
+                    level -= 1
+                if event.key == pygame.K_LEFT:
+                    scroll_left = True
+                if event.key == pygame.K_RIGHT:
+                    scroll_right = True
+                if event.key == pygame.K_LSHIFT:
+                    scroll_speed = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    scroll_left = False
+                if event.key == pygame.K_RIGHT:
+                    scroll_right = False
+                if event.key == pygame.K_LSHIFT:
+                    scroll_speed = 1
+        pygame.display.update()
+        surf = pygame.transform.scale(display, WINDOW_SIZE)
+        screen.blit(surf, (0, 0))
+    pygame.quit()
+
+LevelEditor()
+
